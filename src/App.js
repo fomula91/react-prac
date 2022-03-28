d1a2ad151d133a883c7121025e8c5620d97ba9df
fomula91 <fomula91@gmail.com>
2022-03-29 03:07:24 +0900

    edit project - react 16.3 step

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content'
+import Subject from './components/Subject'
 import './App.css';
 
-class Subject extends Component {
-  render(){
-    return (
-      <header>
-            <h1>{this.props.title}</h1>
-            {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-            <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">Java script</a></li>
-            </ul>
-        </nav>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-      <h2>{this.props.title}</h2>
-      {this.props.desc}
-      </article>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'read',
+      subject:{title: 'Web', sub: 'world wide Web'},
+      welcome: {title:'Welcome!', desc:'hello React!!!'},
+      content: [
+        {id: 1, title: 'HTML', desc: 'HTML is for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
+    console.log('app render')
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if(this.state.mode === 'read'){
+      _title = this.state.content[0].title;
+      _desc = this.state.content[0].desc;
+    }
     return (
+      
       <div class="App">
       hello, react!!
-      <Subject title="WEB" sub="world wide Web!"></Subject>
-      <Subject title="React" sub="For UI"></Subject>
-      <TOC></TOC>
-      <Content title="HTML" desc="HTML is HyperText Markup language"></Content>
+        <header>
+              <h1><a href="/" onClick={function(e){
+                console.log(e)
+                e.preventDefault();
+                // this.state.mode = 'welcome';
+                this.setState({
+                  mode: 'welcome'
+                })
+              }.bind(this)}>{this.state.subject.title}</a></h1>
+              {this.state.subject.sub}
+        </header>
+      {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+      
+      <TOC data={this.state.content}></TOC>
+      <Content title={_title} desc={_desc}></Content>
 
       </div>
     );
