96d88cf876fa874549e91e601185329c4d64a910
fomula91 <fomula91@gmail.com>
2022-03-31 22:33:56 +0900

    edit useEffect clone

--- react-nomade-webservice/src/App.js
+++ react-nomade-webservice/src/App.js
@@ -1,11 +1,29 @@
-import Button from './Button';
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
+  const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => {
+    setValue((prev) => prev + 1)
+  }
+  const onChange = (event) => { 
+      setKeyword(event.target.value)
+  }
+  console.log("i run all the time");
+ 
+  useEffect(() => {
+    console.log("Call th api....")
+  },[]);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+    console.log("serct for", keyword)
+    }
+  }, [keyword])
+  return ( 
     <div>
-      <h1 className={styles.title}>welcome Back!!!</h1>
-      <Button text={"Continue"}/>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
+      <h1> {counter} </h1>
+      <button onClick={onClick}> click me</button>
     </div>
   );
 }
