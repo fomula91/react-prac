53d3916a7096ea8059946576d6a72aff03084f5c
fomula91 <fomula91@gmail.com>
2022-04-02 01:25:27 +0900

    edit todolist features

--- react-todolist-prac/src/App.js
+++ react-todolist-prac/src/App.js
@@ -1,16 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 function App() {
   const [todo, setTodo] = useState("");
-  
-  console.log(todo)
+  const [todolist, setTodolist] = useState([])
+  const handleChange= (e) => {
+    setTodo(e.target.value)
+  }
+  const handleSubmit= (e) => {
+    console.log(e.target.value)
+    setTodolist((todolist) => [...todolist, todo])
+    localStorage.setItem("todoInLocal", todolist)
+    
+  }
+  useEffect((e) => {
+    console.log(e)
+  },[todo])
+  console.log(localStorage.getItem("todoInLocal"))
+  const test = () => {
+    
+    return <ul>
+      <li>hello world</li>
+      <button>delete</button>
+      <li>list1... {localStorage.getItem("todoInLocal")}<button>delete</button></li>
 
+    </ul>
+  }
   return (
     <div>
-      <h1> My Todo App</h1>
-      <input value={todo} placeholder="input your todo list..."></input>
-      <button>add here!</button>
+        <h1> My Todo App</h1>
+        <input onChange={handleChange} value={todo} placeholder="input your todo list..."></input>
+        <button onClick={handleSubmit} value={todo}>add here!</button>
       <hr/>
-
+        <div>
+              {test()}
+        </div>
     </div>
   );
 }
